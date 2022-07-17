f0e37aa5ace4a22509e82bf335e22f8e800008cd
Vol Ky <vol@wilab.org.ua>
2022-07-17 21:48:14 +0300

    sum: move conv initialization to init_1d_kernel_

--- nsf/sum.py
+++ nsf/sum.py
@@ -1,4 +1,5 @@
 from scipy.signal import remez
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -8,6 +9,11 @@ class kernels:
     lpf_u = remez(11, bands=[0, 1000, 3000, 8000], desired=[1, 0], weight=[1, 24], fs=16000)
     hpf_v = remez(11, bands=[0, 5000, 7000, 8000], desired=[0, 1], weight=[24, 1], fs=16000)
     hpf_u = remez(9, bands=[0, 1000, 3000, 8000], desired=[0, 1], weight=[24, 1], fs=16000)
+
+
+def init_1d_kernel_(conv, kernel, requires_grad=False):
+    conv.weight.data = torch.from_numpy(kernel)[None, None, :].float()
+    conv.requires_grad_(requires_grad)
 
 
 class Sum(nn.Module):
@@ -22,26 +28,22 @@ class Sum(nn.Module):
 
         self.harmonic_v = nn.Conv1d(channels, channels, kernel_size=len(kernels.lpf_v),
                                     groups=channels, bias=False)
-        self.harmonic_v.weight.data = kernels.lpf_v
-        self.harmonic_v.requires_grad_(False)
+        init_1d_kernel_(self.harmonic_v, kernels.lpf_v)
         self.harmonic_v_padding = len(kernels.lpf_v) - 1
 
         self.harmonic_u = nn.Conv1d(channels, channels, kernel_size=len(kernels.lpf_u),
                                     groups=channels, bias=False)
-        self.harmonic_u.weight.data = kernels.lpf_u
-        self.harmonic_u.requires_grad_(False)
+        init_1d_kernel_(self.harmonic_u, kernels.lpf_u)
         self.harmonic_u_padding = len(kernels.lpf_u) - 1
 
         self.noisy_v = nn.Conv1d(channels, channels, kernel_size=len(kernels.hpf_v),
                                  groups=channels, bias=False)
-        self.noisy_v.weight.data = kernels.hpf_v
-        self.noisy_v.requires_grad_(False)
+        init_1d_kernel_(self.noisy_v, kernels.hpf_v)
         self.noisy_v_padding = len(kernels.hpf_v) - 1
 
         self.noisy_u = nn.Conv1d(channels, channels, kernel_size=len(kernels.hpf_u),
                                  groups=channels, bias=False)
-        self.noisy_u.weight.data = kernels.hpf_u
-        self.noisy_u.requires_grad_(False)
+        init_1d_kernel_(self.noisy_u, kernels.hpf_u)
         self.noisy_u_padding = len(kernels.hpf_u) - 1
 
     def forward(self, uv, harmonic, noisy):
