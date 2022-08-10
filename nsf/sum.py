14cf86ba176c88b430d9810c87fc530e4e398887
Volodymyr Kyrylov <vol@wilab.org.ua>
2022-08-10 12:50:14 +0300

    init_1d_fir_kernel_: reverse the kernel to get the weight

--- nsf/sum.py
+++ nsf/sum.py
@@ -11,8 +11,9 @@ class kernels:
     hpf_u = remez(9, bands=[0, 1000, 3000, 8000], desired=[0, 1], weight=[24, 1], fs=16000)
 
 
-def init_1d_kernel_(conv, kernel, requires_grad=False):
-    conv.weight.data = torch.from_numpy(kernel)[None, None, :].float()
+def init_1d_fir_kernel_(conv, kernel, requires_grad=False):
+    weight = kernel[::-1]
+    conv.weight.data = torch.from_numpy(weight)[None, None, :].float()
     conv.requires_grad_(requires_grad)
 
 
@@ -28,22 +29,22 @@ class Sum(nn.Module):
 
         self.harmonic_v = nn.Conv1d(channels, channels, kernel_size=len(kernels.lpf_v),
                                     groups=channels, bias=False)
-        init_1d_kernel_(self.harmonic_v, kernels.lpf_v)
+        init_1d_fir_kernel_(self.harmonic_v, kernels.lpf_v)
         self.harmonic_v_padding = len(kernels.lpf_v) - 1
 
         self.harmonic_u = nn.Conv1d(channels, channels, kernel_size=len(kernels.lpf_u),
                                     groups=channels, bias=False)
-        init_1d_kernel_(self.harmonic_u, kernels.lpf_u)
+        init_1d_fir_kernel_(self.harmonic_u, kernels.lpf_u)
         self.harmonic_u_padding = len(kernels.lpf_u) - 1
 
         self.noisy_v = nn.Conv1d(channels, channels, kernel_size=len(kernels.hpf_v),
                                  groups=channels, bias=False)
-        init_1d_kernel_(self.noisy_v, kernels.hpf_v)
+        init_1d_fir_kernel_(self.noisy_v, kernels.hpf_v)
         self.noisy_v_padding = len(kernels.hpf_v) - 1
 
         self.noisy_u = nn.Conv1d(channels, channels, kernel_size=len(kernels.hpf_u),
                                  groups=channels, bias=False)
-        init_1d_kernel_(self.noisy_u, kernels.hpf_u)
+        init_1d_fir_kernel_(self.noisy_u, kernels.hpf_u)
         self.noisy_u_padding = len(kernels.hpf_u) - 1
 
     def forward(self, uv, harmonic, noisy):
